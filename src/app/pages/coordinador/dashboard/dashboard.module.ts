59ea6d8c50550e9a760175e1b745e94c8e2e200e
David Cruz <100491557+Cruz1229@users.noreply.github.com>
2025-06-05 00:00:47 -0600

    feat: añadir pantallas de coordinador
    * feat: Implementar sidebar por rol
    
    * feat: añadir pantallas de coordinador

--- src/app/pages/coordinador/dashboard/dashboard.module.ts
+++ src/app/pages/coordinador/dashboard/dashboard.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-import { ModalsModule, WidgetsModule } from '../../../template/widgets';
+import { DropdownMenusModule, ModalsModule, WidgetsModule } from '../../../template/widgets';
+import { SharedModule } from 'src/app/template/shared/shared.module';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
     declarations: [DashboardComponent],
@@ -16,6 +18,9 @@ import { ModalsModule, WidgetsModule } from '../../../template/widgets';
         ]),
         WidgetsModule,
         ModalsModule,
+        SharedModule,
+        DropdownMenusModule,
+        NgApexchartsModule
     ],
 })
 export class DashboardModule { }
