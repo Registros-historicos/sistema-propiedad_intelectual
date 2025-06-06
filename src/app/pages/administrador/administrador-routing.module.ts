604e697e4e9ab14337aa049bb376eb902ea2e648
Jesus Ruiz <109308593+DroimerGamer@users.noreply.github.com>
2025-06-06 15:37:01 -0600

    feat: Implementar cambios de tablas y creacion de cord y soli

--- src/app/pages/administrador/administrador-routing.module.ts
+++ src/app/pages/administrador/administrador-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CoordinatorListingComponent} from './coordinator-listing/coordinator-listing.component';
-import {ApplicantListingComponent} from './applicant-listing/applicant-listing.component';
+import { CoordinatorListingComponent } from './coordinator-listing/coordinator-listing.component';
+import { CoordinatorFormComponent } from './coordinator-form/coordinator-form.component';
+import { ApplicantListingComponent } from './applicant-listing/applicant-listing.component';
+import {ApplicantFormComponent} from './applicant-form/applicant-form.component';
 
 const routes: Routes = [
   {
@@ -17,12 +19,24 @@ const routes: Routes = [
       },
       {
         path: 'coordinadores',
-        loadComponent: () => import('./coordinator-listing/coordinator-listing.component').then(m => m.CoordinatorListingComponent)
+        component: CoordinatorListingComponent
+      },
+      {
+        path: 'coordinadores/create',
+        component: CoordinatorFormComponent
+      },
+      {
+        path: 'coordinadores/edit/:id',
+        component: CoordinatorFormComponent
       },
       {
         path: 'solicitantes',
-        loadComponent: () => import('./applicant-listing/applicant-listing.component').then(m => m.ApplicantListingComponent)
-      }
+        component: ApplicantListingComponent
+      },
+      {
+        path: 'solciitantes/create',
+        component: ApplicantFormComponent
+      },
     ]
   }
 ];
--- src/app/pages/administrador/administrador.module.ts
+++ src/app/pages/administrador/administrador.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdministradorRoutingModule } from './administrador-routing.module';
-import {FormsModule} from '@angular/forms';
-import {NgbCollapseModule, NgbModalModule, NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
-import {SweetAlert2Module} from '@sweetalert2/ngx-sweetalert2';
-import {ApplicantListingComponent} from './applicant-listing/applicant-listing.component';
-import {CoordinatorListingComponent} from './coordinator-listing/coordinator-listing.component';
-import {ApplicantService, CoordinatorService, InstitucionService} from './shared-services';
-
+import { FormsModule } from '@angular/forms';
+import { NgbCollapseModule, NgbModalModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { ApplicantListingComponent } from './applicant-listing/applicant-listing.component';
+import { ApplicantFormComponent } from './applicant-form/applicant-form.component';
+import { CoordinatorListingComponent } from './coordinator-listing/coordinator-listing.component';
+import { CoordinatorFormComponent } from './coordinator-form/coordinator-form.component';
+import { ApplicantService, CoordinatorService, InstitucionService } from './shared-services';
 
 @NgModule({
   declarations: [],
@@ -23,7 +24,9 @@ import {ApplicantService, CoordinatorService, InstitucionService} from './shared
 
     SweetAlert2Module.forChild(),
     ApplicantListingComponent,
+    ApplicantFormComponent,
     CoordinatorListingComponent,
+    CoordinatorFormComponent,
   ],
   providers: [
     CoordinatorService,
