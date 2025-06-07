7a018dfb26510f2f54ae83f71d8e390663d7956b
David Cruz <davidflres29@gmail.com>
2025-06-06 19:13:24 -0600

    feat: opciones de sidebar para coordinador

--- src/app/template/layout/components/sidebar/sidebar-menu/sidebar-menu.component.ts
+++ src/app/template/layout/components/sidebar/sidebar-menu/sidebar-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ADMINISTRATOR_MENUS} from '../../../../shared/menus';
+import { COORDINATOR_MENUS } from '../../../../shared/menus';
 
 @Component({
   selector: 'app-sidebar-menu',
@@ -19,6 +20,7 @@ export class SidebarMenuComponent implements OnInit {
         this.menuItems = ADMINISTRATOR_MENUS;
         break;
       case 2:
+        this.menuItems = COORDINATOR_MENUS;
         break;
       case 3:
         break;
