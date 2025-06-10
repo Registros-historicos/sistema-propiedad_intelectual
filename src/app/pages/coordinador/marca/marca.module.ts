6005e7c8c079caa7eb6fcef6229476fe49f636f7
David Cruz <davidflres29@gmail.com>
2025-06-09 23:34:21 -0600

    A feat: tabla de patentes con 150 registros de prueba

--- src/app/pages/coordinador/marca/marca.module.ts
+++ src/app/pages/coordinador/marca/marca.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MarcaComponent } from './marca.component';
-import { ModalsModule, WidgetsModule } from '../../../template/widgets';
+import { FormsModule } from '@angular/forms';
+import { ModalsModule, WidgetsModule } from 'src/app/template/widgets';
+import { SharedModule } from 'src/app/template/shared/shared.module';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { CrudModule } from 'src/app/modules/crud/crud.module';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
     declarations: [MarcaComponent],
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule.forChild([
             {
                 path: '',
@@ -16,6 +22,10 @@ import { ModalsModule, WidgetsModule } from '../../../template/widgets';
         ]),
         WidgetsModule,
         ModalsModule,
+        SharedModule,
+        NgbCollapseModule,
+        CrudModule,
+        SweetAlert2Module.forChild(),
     ],
 })
 export class MarcaModule { }
--- src/app/pages/coordinador/patentes/patentes.module.ts
+++ src/app/pages/coordinador/patentes/patentes.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PatentesComponent } from './patentes.component';
 import { ModalsModule, WidgetsModule } from '../../../template/widgets';
+import { FormsModule } from '@angular/forms';
+import { CrudModule } from 'src/app/modules/crud/crud.module';
+import { TableComponent } from './table/table.component';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { SharedModule } from 'src/app/template/shared/shared.module';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    declarations: [PatentesComponent],
+    declarations: [PatentesComponent, TableComponent],
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule.forChild([
             {
                 path: '',
@@ -16,6 +23,10 @@ import { ModalsModule, WidgetsModule } from '../../../template/widgets';
         ]),
         WidgetsModule,
         ModalsModule,
+        CrudModule,
+        SweetAlert2Module.forChild(),
+        SharedModule,
+        NgbCollapseModule
     ],
 })
 export class PatentesModule { }
