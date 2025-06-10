090973185348599c0c9680fb4e239d0fa35db459
David Cruz <davidflres29@gmail.com>
2025-06-09 23:34:21 -0600

    feat: tabla de patentes con 150 registros de prueba

--- src/app/pages/coordinador/solicitantes/solicitantes.module.ts
+++ src/app/pages/coordinador/solicitantes/solicitantes.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SolicitantesComponent } from './solicitantes.component';
 import { ModalsModule, WidgetsModule } from '../../../template/widgets';
+import { SharedModule } from 'src/app/template/shared/shared.module';
+import { CrudModule } from 'src/app/modules/crud/crud.module';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { FormsModule } from '@angular/forms';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [SolicitantesComponent],
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule.forChild([
             {
                 path: '',
@@ -16,6 +22,10 @@ import { ModalsModule, WidgetsModule } from '../../../template/widgets';
         ]),
         WidgetsModule,
         ModalsModule,
+        SharedModule,
+        NgbCollapseModule,
+        CrudModule,
+        SweetAlert2Module.forChild(),
     ],
 })
 export class SolicitantesModule { }
