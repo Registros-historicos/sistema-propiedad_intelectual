a7714e7ec789768024b8faa45c7ac5f90b376966
Braulio David Hernández Palagot <bdpalagot@gmail.com>
2025-06-10 12:42:56 -0600

    Actualizacion de nombres de intituciones.

--- src/app/pages/administrador/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/administrador/admin-dashboard/admin-dashboard.component.ts
@@ -15,19 +15,19 @@ export class AdminDashboardComponent implements OnInit {
   ];
 
   protected readonly topFiveFederalInstitutions = [
-    {name: 'Instituto Nacional de Migración', value: 800},
-    {name: 'Secretaría de Relaciones Exteriores', value: 700},
-    {name: 'Secretaría de Gobernación', value: 600},
-    {name: 'Secretaría de la Defensa Nacional', value: 500},
-    {name: 'Secretaría de Marina', value: 400}
+    {name: 'Instituto Tecnológico de Tuxtla Gutierrez', value: 800},
+    {name: 'Instituto Tecnológico de Durango', value: 700},
+    {name: 'Instituto Tecnológico de Orizaba', value: 600},
+    {name: 'Instituto Tecnológico de Celaya', value: 500},
+    {name: 'Instituto Tecnológico de Acapulco', value: 400}
   ];
 
   protected readonly topFiveCentralizedInstitutions = [
-    {name: 'Gobierno del Estado de Veracruz', value: 400},
-    {name: 'Gobierno del Estado de Puebla', value: 350},
-    {name: 'Gobierno del Estado de Oaxaca', value: 300},
-    {name: 'Gobierno del Estado de Chiapas', value: 250},
-    {name: 'Gobierno del Estado de Tabasco', value: 200}
+    {name: 'Instituto Tecnológico Superior de Zongolica', value: 400},
+    {name: 'Instituto Tecnológico Superior de Palenque', value: 350},
+    {name: 'Instituto Tecnológico Superior de Irapuato', value: 300},
+    {name: 'Instituto Tecnológico Superior de Cintapala', value: 250},
+    {name: 'Instituto Tecnológico Superior de Comitán', value: 200}
   ];
 
   constructor() { }
