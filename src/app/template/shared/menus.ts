b318a6fcf5fd1cd069b00797741a9d87cce657ff
Cris <chris_49git@outlook.com>
2025-06-18 23:29:23 -0600

    fix menu

--- src/app/template/shared/menus.ts
+++ src/app/template/shared/menus.ts
@@ -231,14 +231,20 @@ export const APPLICANTS_MENU = [
   },
   {
     name: 'MENU.APPLICANT.MYPROFILE',
-    icon: 'profile-user',
+    icon: 'person',
     type: 'link',
     link: '/solicitante/dashboard',
   },
   {
     name: 'MENU.APPLICANT.REQUEST',
-    icon: 'element-11',
+    icon: 'description',
     type: 'link',
     link: '/solicitante/registrar',
+  },
+  {
+    name: 'MENU.REPORTS',
+    icon: 'analytics',
+    link: 'solicitante/reportes',
+    type: 'link'
   }
 ];
