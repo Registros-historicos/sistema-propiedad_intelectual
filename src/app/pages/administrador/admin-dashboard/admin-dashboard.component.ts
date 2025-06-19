1884971a38377679c57c8b282b31df94df40d392
Braulio David Hernández Palagot <bdpalagot@gmail.com>
2025-06-19 01:13:56 -0600

    FIX: Unificar el dashboard de Coordinador y Administrador && Agregar los iconos seleccionados en el menú y en el dashboard

--- src/app/pages/administrador/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/administrador/admin-dashboard/admin-dashboard.component.ts
@@ -7,30 +7,91 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
   protected readonly topFiveEntities = [
-    {name: 'Veracruz', value: 800},
-    {name: 'Puebla', value: 700},
-    {name: 'Oaxaca', value: 600},
-    {name: 'Chiapas', value: 500},
-    {name: 'Tabasco', value: 400}
+    {
+      name: 'Veracruz', value: 800, tags: [
+        'PA', 'DA', 'MU', 'DI', 'MA'
+      ]
+    },
+    {
+      name: 'Puebla', value: 700, tags: [
+        'PA', 'MU', 'MA'
+      ]
+    },
+    {
+      name: 'Oaxaca', value: 600, tags: [
+        'PA', 'DA', 'DI', 'MA'
+      ]
+    },
+    {
+      name: 'Chiapas', value: 500, tags: [
+        'DA', 'MU', 'DI'
+      ]
+    },
+    {
+      name: 'Tabasco', value: 400, tags: [
+        'PA', 'MU', 'MA'
+      ]
+    }
   ];
 
   protected readonly topFiveFederalInstitutions = [
-    {name: 'Instituto Tecnológico de Tuxtla Gutierrez', value: 800},
-    {name: 'Instituto Tecnológico de Durango', value: 700},
-    {name: 'Instituto Tecnológico de Orizaba', value: 600},
-    {name: 'Instituto Tecnológico de Celaya', value: 500},
-    {name: 'Instituto Tecnológico de Acapulco', value: 400}
+    {
+      name: 'Instituto Tecnológico de Tuxtla Gutierrez', value: 800, tags: [
+        'PA', 'DA', 'MU', 'DI', 'MA'
+      ]
+    },
+    {
+      name: 'Instituto Tecnológico de Durango', value: 700, tags: [
+        'PA', 'MU', 'MA'
+      ]
+    },
+    {
+      name: 'Instituto Tecnológico de Orizaba', value: 600, tags: [
+        'PA', 'DA', 'DI', 'MA'
+      ]
+    },
+    {
+      name: 'Instituto Tecnológico de Celaya', value: 500, tags: [
+        'DA', 'MU', 'DI'
+      ]
+    },
+    {
+      name: 'Instituto Tecnológico de Acapulco', value: 400, tags: [
+        'PA', 'MU', 'MA'
+      ]
+    }
   ];
 
   protected readonly topFiveCentralizedInstitutions = [
-    {name: 'Instituto Tecnológico Superior de Zongolica', value: 400},
-    {name: 'Instituto Tecnológico Superior de Palenque', value: 350},
-    {name: 'Instituto Tecnológico Superior de Irapuato', value: 300},
-    {name: 'Instituto Tecnológico Superior de Cintapala', value: 250},
-    {name: 'Instituto Tecnológico Superior de Comitán', value: 200}
+    {
+      name: 'Instituto Tecnológico Superior de Zongolica', value: 400, tags: [
+        'PA', 'DA', 'MU', 'DI', 'MA'
+      ]
+    },
+    {
+      name: 'Instituto Tecnológico Superior de Palenque', value: 350, tags: [
+        'PA', 'DA', 'DI', 'MA'
+      ]
+    },
+    {
+      name: 'Instituto Tecnológico Superior de Irapuato', value: 300, tags: [
+        'PA', 'MU', 'MA'
+      ]
+    },
+    {
+      name: 'Instituto Tecnológico Superior de Cintapala', value: 250, tags: [
+        'DA', 'MU', 'DI'
+      ]
+    },
+    {
+      name: 'Instituto Tecnológico Superior de Comitán', value: 200, tags: [
+        'PA', 'DA', 'DI'
+      ]
+    }
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
