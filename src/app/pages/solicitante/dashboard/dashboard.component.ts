f1f823620c21ab2bb4b9022bc9c1f98b51cc5400
Cris <chris_49git@outlook.com>
2025-06-21 14:28:01 -0600

    feat: Refactorizar dashboard - Añadido componente y módulo de perfil con CRUD y modal. - Agregada ruta y enlaces de navegación al perfil. - Actualizadas traducciones en español. - Refactorizado layout y lógica del dashboard.

--- src/app/pages/solicitante/dashboard/dashboard.component.ts
+++ src/app/pages/solicitante/dashboard/dashboard.component.ts
@@ -1,43 +1,19 @@
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-  TemplateRef,
-} from '@angular/core';
-import { Config } from 'datatables.net';
-import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
-import { SweetAlertOptions } from 'sweetalert2';
-import moment from 'moment/moment';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
 import { getCSSVariableValue } from 'src/app/template/kt/_utils';
-import { APPLICANTS_REQUEST_DATA } from 'src/app/api/data/applicant.data';
-import { DerechosAutorComponent } from '../registrar/derechos-autor/derechos-autor.component';
-import { MatDialog } from '@angular/material/dialog'; // Usado para abrir el modal
-import { ApplicantsService } from 'src/app/api/services/applicant.service';
-import { IAplicantModel } from 'src/app/api/models/applicant.model';
-import { Subscription } from 'rxjs'; // Importado para manejar las suscripciones
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
-  pageLength: number = 10;
-  dtInstance: any;
-  lengthMenu: number[] = [5, 10, 15, 20];
-  datatableConfig: Config = {};
-  reloadEvent: EventEmitter<boolean> = new EventEmitter();
-
-  @ViewChild('noticeSwal')
-  noticeSwal!: SwalComponent;
-  @ViewChild('formModal') formModalRef!: TemplateRef<any>;
-
-  swalOptions: SweetAlertOptions = {};
+export class DashboardComponent implements OnInit {
+  tiposSolicitudes = [
+    { nombre: 'Patentes', total: 90, icono: 'emoji_objects' },
+    { nombre: 'Marcas', total: 80, icono: 'credit_card' },
+    { nombre: 'Modelos de Utilidad', total: 70, icono: 'build' },
+    { nombre: 'Derechos de Autor', total: 60, icono: 'copyright' },
+    { nombre: 'Diseños Industriales', total: 50, icono: 'architecture' },
+  ];
 
   solicitudes: any[] = [];
 
@@ -50,94 +26,7 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
   solicitudesConObservaciones: number = 0;
   solicitudesAprobadas: number = 0;
 
-  solicitudSeleccionada: IAplicantModel | undefined;
-
-  isCollapsed1 = false;
-  isCollapsed2 = true;
-  estadoSeleccionado: number | null = null;
-  institucionSeleccionada: number | null = null;
-  selectedFile: File | null = null;
-  isViewMode: boolean = false;
-
-  private applicantSubscription: Subscription | undefined;
-
-  constructor(
-    private cdr: ChangeDetectorRef,
-    private translate: TranslateService,
-    private dialog: MatDialog,
-    private service: ApplicantsService
-  ) {}
-
   ngOnInit(): void {
-    this.solicitudes = APPLICANTS_REQUEST_DATA.map(
-      (applicant: IAplicantModel) => ({
-        id: applicant.id,
-        tipo: applicant.titulo,
-        titulo: applicant.descripcion,
-        estado: applicant.estado,
-        fecha: applicant.fechaSolicitud,
-      })
-    );
-
-    this.datatableConfig = {
-      serverSide: false,
-      lengthMenu: this.lengthMenu,
-      pageLength: this.pageLength,
-      language: {
-        info: this.translate.instant('TABLE.PAG_INFO'),
-        infoFiltered: this.translate.instant('TABLE.PAG_INFO_FILTERED'),
-        processing: this.translate.instant('TABLE.PROCESSING'),
-        emptyTable: this.translate.instant('TABLE.EMPTY_TABLE'),
-        infoEmpty: this.translate.instant('TABLE.PAG_INFO_EMPTY'),
-        zeroRecords: this.translate.instant('TABLE.ZERO_RECORDS'),
-      },
-      data: this.solicitudes,
-      columns: [
-        {
-          title: this.translate.instant('TABLE.TYPE_REQUEST'),
-          data: 'tipo',
-          render: (data) =>
-            `<span class="fw-bold fs-6 text-gray-800">${data || ''}</span>`,
-        },
-        {
-          title: this.translate.instant('TABLE.WORK_TITLE'),
-          data: 'titulo',
-          render: (data) =>
-            `<span class="fw-semibold text-gray-600">${data || ''}</span>`,
-        },
-        {
-          title: this.translate.instant('TABLE.STATUS_REQUEST'),
-          data: 'estado',
-          render: (data) =>
-            `<span class="fw-semibold text-gray-600">${data || ''}</span>`,
-        },
-        {
-          title: this.translate.instant('TABLE.DATE'),
-          data: 'fecha',
-          render: (data) =>
-            `<span class="fw-semibold text-gray-600">${moment(data).format(
-              'DD-MM-YYYY'
-            )}</span>`,
-        },
-      ],
-      createdRow: (row, data: any, dataIndex) => {
-        const $row = $(row);
-        $row.attr('data-action', 'view');
-        $row.attr('data-id', data.id);
-        $row.addClass('cursor-pointer');
-      },
-      initComplete: (settings, json) => {
-        this.dtInstance = settings.oInstance.api();
-        this.cdr.detectChanges();
-        $(this.dtInstance.table().body()).on('click', 'tr', (event: any) => {
-          const rowData = this.dtInstance.row(event.currentTarget).data();
-          if (rowData && rowData.id) {
-            this.view(rowData.id);
-          }
-        });
-      },
-    };
-
     const solicitudesData = [
       { tipo: 'DA', estado: 'Pendiente', data: [8, 12, 10, 10, 8, 12] },
       { tipo: 'PA', estado: 'En trámite', data: [4, 6, 8, 6, 4, 6] },
@@ -203,17 +92,6 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
       );
 
     this.chartOptions = this.getChartOptions(350);
-  }
-
-  onPageLengthChange(event: any): void {
-    const newLength = parseInt(event.target.value);
-    this.pageLength = newLength;
-
-    if (this.dtInstance) {
-      this.dtInstance.page.len(newLength).draw();
-    } else {
-      this.reloadEvent.emit(true);
-    }
   }
 
   getChartOptions(height: number) {
@@ -306,72 +184,4 @@ export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
       },
     };
   }
-
-  delete(event: any): void {
-    this.showAlert({
-      title: '¿Estás seguro?',
-      text: 'Esta acción eliminará la solicitud.',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Sí, eliminar',
-      cancelButtonText: 'Cancelar',
-    });
-  }
-
-  editarSolicitud(id: number): void {
-    const solicitud = this.solicitudes.find((sol) => sol.id === id);
-
-    if (solicitud) {
-      this.solicitudSeleccionada = solicitud;
-
-      this.dialog.open(DerechosAutorComponent, {
-        width: '800px',
-        data: { solicitud: this.solicitudSeleccionada },
-      });
-    } else {
-      console.error('Solicitud no encontrada');
-    }
-  }
-
-  showAlert(swalOptions: SweetAlertOptions): void {
-    this.swalOptions = Object.assign(
-      {
-        buttonsStyling: false,
-        confirmButtonText: 'Ok, entendido!',
-        customClass: {
-          confirmButton: 'btn btn-success',
-        },
-      },
-      swalOptions
-    );
-    this.cdr.detectChanges();
-    this.noticeSwal.fire();
-  }
-
-  view(id: number) {
-    if (this.applicantSubscription) {
-      this.applicantSubscription.unsubscribe();
-    }
-
-    this.applicantSubscription = this.service.getApplicant(id).subscribe(
-      (applicantData: IAplicantModel) => {
-        this.solicitudSeleccionada = { ...applicantData };
-        this.isViewMode = true;
-      },
-      (error) => {
-        console.error('Error al cargar la solicitud:', error);
-        this.solicitudSeleccionada = undefined;
-      }
-    );
-  }
-
-  ngAfterViewInit(): void {}
-
-  ngOnDestroy(): void {
-    if (this.applicantSubscription) {
-      this.applicantSubscription.unsubscribe();
-    }
-    this.reloadEvent.unsubscribe();
-  }
-
 }
