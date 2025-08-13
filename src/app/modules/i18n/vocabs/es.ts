08a2dcd6c6e10f8eae6f7843290293b755fea4aa
Chris <104177004+Chris-THC@users.noreply.github.com>
2025-08-12 23:12:29 -0600

    Feature graph by year
    * Feat: Agregar gráficos históricos de IMPI e INDAUTOR en el panel de administrador
    
    * Visualizar contenido Excel (#80)
    
    * Se realizaron los cambios para poder previsualizar el contenido de los excels y se añadio el boton para descargar plantilla. Ademas se añade un excel de prueba de IMPI de descarga. Cuando se tengan los 2 excels de plantilla se añadiran dentro de la carpeta creada para estos.
    
    * feat: se agrego la validacion para guardar los datos excel
    
    ---------
    
    Co-authored-by: Daniela Espinosa <daniela.espinosa.3103@gmail.com>
    
    * feat: Generalizar top de instituciones (#81)
    
    * feat: Generalizar top de instituciones
    
    * feat: Agregar gráficos históricos de IMPI e INDAUTOR en el tablero de administrador
    
    * Revert "feat: Generalizar top de instituciones"
    
    This reverts commit 5b598ba8380cd3a115f010b97a31909ec931690e.
    
    * Revertir a commit 5b598ba
    
    ---------
    
    Co-authored-by: Cris <chris_49git@outlook.com>
    
    * Visualizar contenido Excel (#80)
    
    * Se realizaron los cambios para poder previsualizar el contenido de los excels y se añadio el boton para descargar plantilla. Ademas se añade un excel de prueba de IMPI de descarga. Cuando se tengan los 2 excels de plantilla se añadiran dentro de la carpeta creada para estos.
    
    * feat: se agrego la validacion para guardar los datos excel
    
    ---------
    
    Co-authored-by: Daniela Espinosa <daniela.espinosa.3103@gmail.com>
    
    * feat: Generalizar top de instituciones (#81)
    
    * feat: Generalizar top de instituciones
    
    * feat: Agregar gráficos históricos de IMPI e INDAUTOR en el tablero de administrador
    
    * Revert "feat: Generalizar top de instituciones"
    
    This reverts commit 5b598ba8380cd3a115f010b97a31909ec931690e.
    
    * Revertir a commit 5b598ba
    
    ---------
    
    Co-authored-by: Cris <chris_49git@outlook.com>
    
    * Git rebase (feature-graph-year|REBASE 1/2)
    
    ---------
    
    Co-authored-by: Jesus Ruiz <109308593+DroimerGamer@users.noreply.github.com>
    Co-authored-by: Daniela Espinosa <daniela.espinosa.3103@gmail.com>
    Co-authored-by: Valeria Contreras Zaragoza <119027389+ValConZ@users.noreply.github.com>

--- src/app/modules/i18n/vocabs/es.ts
+++ src/app/modules/i18n/vocabs/es.ts
@@ -1038,6 +1038,8 @@ export const locale = {
         INSTITUTIONS: "Instituciones",
         APPLICATION_TYPE: "Tipo de solicitud",
         FEDERAL_ENTITIES: "Entidades Federativas",
+        HISTORICAL_REGISTERS_INDAUTOR:"Registros Históricos INDAUTOR",
+        HISTORICAL_REGISTERS_IMPI:"Registros Históricos IMPI",
       },
       SUBTITLES: {
         REGISTER: "Registros",
