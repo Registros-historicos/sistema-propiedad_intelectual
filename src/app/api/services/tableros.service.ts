7f55d46512cd401d8b74fed2dfd57404e55aec4d
Andrea Beelegui Coss Espinosa <59074955+Andoria-Coss@users.noreply.github.com>
2025-10-04 20:37:09 -0600

    Conectar el tablero de entidades federativas al backend (#109)
    Se conectó el tablero al backend para mostrar el top 10

---
+++ src/app/api/services/tableros.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TablerosService {
+  constructor(private http: HttpClient) {}
+
+  getTopEntities(): Observable<any[]> {
+    return this.http.get<any[]>('/api/tableros/entidades/top10');
+  }
+    // NUEVO MÉTODO PARA INSTITUCIONES
+  getTopInstitutions(): Observable<any[]> {
+    return this.http.get<any[]>('/api/tableros/instituciones/top10');
+  }
+}
