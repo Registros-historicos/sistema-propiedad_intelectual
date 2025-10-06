55c25c212ef9dc7537adc89c9a26172958c08fa7
Daniela Espinosa <daniela.espinosa.3103@gmail.com>
2025-10-05 22:16:09 -0600

    fix: merge develop

--- src/app/api/services/tableros.service.ts
+++ src/app/api/services/tableros.service.ts
@@ -25,8 +25,9 @@ export class TablerosService {
   // NUEVO MÉTODO PARA CATEGORÍAS DE INVESTIGADORES
   getCategoriasInvestigadores(): Observable<CategoriaInvestigador[]> {
     return this.http.get<CategoriaInvestigador[]>('/api/tableros/investigadores/categorias/');
-   //Método para Registro por Estatus
-  getRegisterStatus(): Observable<any[]> {
+  }
+  
+   getRegisterStatus(): Observable<any[]> {
     return this.http.get<any[]>('/api/tableros/registros/estatus');
   }
 }
