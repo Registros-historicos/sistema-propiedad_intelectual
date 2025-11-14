324547a19d9b7d784f9ce79797869833540fb167
Chris <104177004+Chris-THC@users.noreply.github.com>
2025-11-13 20:48:44 -0600

    Feature/create new coordinador (#212)
    * Implementa la actualización de la institución del coordinador tras su creación, mejorando la gestión de errores y la lógica de flujo.

---
+++ src/app/api/services/coordinador.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+export interface UserCoordinator {
+  id_usuario?: number;
+  nombre: string;
+  ape_pat: string;
+  ape_mat: string;
+  url_foto: string;
+  correo: string;
+  password?: string;
+  telefono: string;
+  tipo_usuario_param: number;
+  estatus: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoordinatorHttpService {
+  constructor(private http: HttpClient) {}
+
+  createNewUserCoordinator(usuario: UserCoordinator): Observable<UserCoordinator> {
+    return this.http.post<UserCoordinator>('/api/usuarios/', usuario).pipe(
+      catchError((error: any) => {
+        const mensaje =
+          error?.error?.message || error.message || 'Error desconocido';
+        return throwError(() => new Error(mensaje));
+      })
+    );
+  }
+
+   updateInstitutionByIdCoordinator(idInstitucion: number, idCoord: number | null): Observable<any> {
+      const url = `/api/institucion/usuario/${idInstitucion}/`;
+      const body = { id_coordinador: idCoord };
+      return this.http.put(url, body).pipe(
+        map(() => ({ status: 200, message: 'Actualización exitosa' })),
+        catchError((error) => {
+          const mensaje =
+            error?.error?.message ||
+            error.message ||
+            'Error al actualizar el id_coordinador';
+          return throwError(() => new Error(mensaje));
+        })
+      );
+    }
+}
